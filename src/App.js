a9eaa72dc4a7d7cbb3247c62141701962f4f6c5a
Havronskyi Nazarii <nazar.gavronsky.mail@gmail.com>
2021-08-23 17:32:49 +0300

    TEST

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      LOOOL
+      LOOOL000
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>

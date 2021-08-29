a17bf3568a6faabf5b58c74d40bda9b6733ffa99
Havronskyi Nazarii <nazar.gavronsky.mail@gmail.com>
2021-08-29 12:42:09 +0300

    Home_7 (done)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import {useReducer} from "react";
+
+
+const reducer =(state, action) => {
+    if (action.obg && action.do === `+10`)
+    {
+        return {...state,first: state.first+10}
+    } else if (action.obg && action.do === `-2`) {
+        return {...state, first: state.first-2}
+    }
+    return state
+}
 
 function App() {
-  return (
-    <div className="App">
-        123
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    const [{first}, dispatch] = useReducer(reducer, {first: 0});
+    return (
+    <div>
+        <h2>{first}</h2>
+        <button onClick={()=>dispatch({obg: `first`, do: `+10`})}>+10</button>
+        <button onClick={()=>dispatch(({obg:`first`, do: `-2`}))}>-2</button>
     </div>
   );
 }

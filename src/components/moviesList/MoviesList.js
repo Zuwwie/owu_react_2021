8dd3e69c9f10f284de7bc682df552d2873718da0
Havronskyi Nazarii <nazar.gavronsky.mail@gmail.com>
2021-09-06 19:23:08 +0300

    v2 fix pagination

--- src/components/moviesList/MoviesList.js
+++ src/components/moviesList/MoviesList.js
@@ -42,12 +42,12 @@ function MoviesList() {
 
                 <Link className={'selected'} to={'/list/' + listId}>{listId}</Link>
 
-                <Link className={'link'} to={'/list/' + (listId+1)}>{listId+1}</Link>
-                <Link className={'link'} to={'/list/' + (listId+2)}>{listId+2}</Link>
-                <Link className={'link'} to={'/list/' + (listId+3)}>{listId+3}</Link>
-                <Link className={'link'} to={'/list/' + (listId+4)}>{listId+4}</Link>
+                {listId<499 && <Link className={'link'} to={'/list/' + (listId + 1)}>{listId + 1}</Link>}
+                {listId<498 && <Link className={'link'} to={'/list/' + (listId + 2)}>{listId + 2}</Link>}
+                {listId<497 && <Link className={'link'} to={'/list/' + (listId + 3)}>{listId + 3}</Link>}
+                {listId<496 && <Link className={'link'} to={'/list/' + (listId + 4)}>{listId + 4}</Link>}
 
-                <Link className={'link'} to={'/list/500'}>500</Link>
+                {listId !== 500 && <Link className={'link'} to={'/list/500'}>500</Link>}
 
 
             </p>
